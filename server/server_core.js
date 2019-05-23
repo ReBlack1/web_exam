a19fc05b5e4722f2023f7a4fdaefb46f590bc07e
ReBlack <reblack1@yandex.ru>
2019-05-23 15:27:12 +0200

    Настроил выдачу главной страницы на сервере Убрал дублирование гравной страницы

--- server/server_core.js
+++ server/server_core.js
@@ -1,12 +1,19 @@
 ﻿// подключение express
 const express = require("express");
+const path = require('path');
 // создаем объект приложения
 const app = express();
+// Директория статики
+const BUILD_DIRECTORY = path.resolve(__dirname, '..', 'client')
+// Настройка статики
+app.use(express.static(BUILD_DIRECTORY));
+
 // определяем обработчик для маршрута "/"
 app.get("/", function (request, response) {
 
         // отправляем ответ
-        response.send("<h2>Привет Express!</h2>");
+    response.sendFile(`${BUILD_DIRECTORY}/auth.html`);
+
 });
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
