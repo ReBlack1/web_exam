1590c3aeda0a237c0057d0b8112a0e22443707aa
ReBlack <reblack1@yandex.ru>
2019-05-26 20:06:50 +0200

    Добавление новых учетных записей, проверки

--- server/database/DB_manager.js
+++ server/database/DB_manager.js
@@ -1,11 +1,12 @@
 ﻿// Подключение БД
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var DB_config = require('./DB_config');
 const login = DB_config.get_login();
 const pwd = DB_config.get_pwd();
 const conn = 'mongodb+srv://' + login + ':' + pwd + '@cluster0-tvj47.mongodb.net/test?retryWrites=true'
 
-mongoose.connect(conn);
+mongoose.connect(conn, { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -13,13 +14,252 @@ db.once('open', function () {
     // we're connected!
     console.log("OK!");
 });
-
-const User = mongoose.model('User', {
+const UserSchema = new Schema({
     nickname: String,
     email: String,
     login: String,
     pwd: String
-});
+})
 
-const user1 = new User({nickname: 'test_nick', email:'test2@mail.ru', login:'test_2login', pwd:"test2_pwd"});
-user1.save().then(() => console.log('meow'));
+/* Наверно можно заменить 
+  {
+        proficiency: String,
+        value: Number
+  }
+  на подсхему, но я не уверен, что это сработает и лень пробовать */
+const CharSchema = new Schema(
+    {
+        img: String,
+        characterName: String,
+        class: String,
+        level: String,
+        race: String,
+        playerName: String,
+        masterName: String,
+        background: String,
+        alignment: String,
+        experiencePoints: String,
+        inspiration: String,
+        proficiencyBonus: String,
+        passiveWisdom: String,
+        characterStats:
+            {
+                strength: String,
+                dexterity: String,
+                constitution: String,
+                intelligence: String,
+                wisdom: String,
+                charisma: String
+            },
+
+        savingThrows:
+            {
+                strength:
+                    {
+                        proficiency: String,
+                        value: String
+                    },
+                dexterity:
+                    {
+                        proficiency: String,
+                        value: String
+                    },
+                intelligence:
+                    {
+                        proficiency: String,
+                        value: String
+                    },
+                wisdom:
+                    {
+                        proficiency: String,
+                        value: String
+                    },
+                charisma:
+                    {
+                        proficiency: String,
+                        value: String
+                    },
+            },
+        skills:
+            {
+                acrobatics: {
+                    proficiency: String,
+                    value: String
+                },
+
+                animalHandling: {
+                    proficiency: String,
+                    value: String
+                },
+
+                arcana: {
+                    proficiency: String,
+                    value: String
+                },
+
+                athletics: {
+                    proficiency: String,
+                    value: String
+                },
+
+                deception: {
+                    proficiency: String,
+                    value: String
+                },
+
+                history: {
+                    proficiency: String,
+                    value: String
+                },
+
+                insight: {
+                    proficiency: String,
+                    value: String
+                },
+
+                intimidation: {
+                    proficiency: String,
+                    value: String
+                },
+
+                investigation: {
+                    proficiency: String,
+                    value: String
+                },
+
+                medicine: {
+                    proficiency: String,
+                    value: String
+                },
+
+                nature: {
+                    proficiency: String,
+                    value: String
+                },
+
+                perception: {
+                    proficiency: String,
+                    value: String
+                },
+
+                performance: {
+                    proficiency: String,
+                    value: String
+                },
+
+                persuasion: {
+                    proficiency: String,
+                    value: String
+                },
+
+                religion: {
+                    proficiency: String,
+                    value: String
+                },
+
+                sleightOfHand: {
+                    proficiency: String,
+                    value: String
+                },
+
+                stealth: {
+                    proficiency: String,
+                    value: String
+                },
+
+                survival: {
+                    proficiency: String,
+                    value: String
+                },
+
+            },
+        state: {
+            armor: String,
+            initiative: String,
+            speed: String,
+
+
+            hitPoints: {
+                maximal: String,
+                current: String,
+                temporary: String
+            },
+
+            hitDice: {
+                total: String,
+                dice: String
+            },
+
+            deathSaves: {
+                successes: String,
+                failures: String
+            }
+        },
+
+
+        attacksAndSpells:
+            [
+                {
+                    name: String,
+                    attackBonus: String,
+
+                    damage: {
+                        dices: String,
+                        dice: String
+                    }
+                }
+            ],
+
+        mindset: {
+            personalityTraits: String,
+
+            ideals: String,
+
+            bonds: String,
+
+            weaknesses: String
+        },
+
+        featuresAndTraits: String,
+
+
+        equipment: {
+
+            coins: {
+                copper: String,
+                silver: String,
+                electrum: String,
+                gold: String,
+                platinum: String
+            },
+
+            equipment: String
+        },
+
+        misc: String
+    })
+
+const User = mongoose.model('User', UserSchema, 'users');
+const Char = mongoose.model("Char", CharSchema, 'chars')
+
+module.exports = {
+    add_user: function (nickname, email, login, pwd) {
+        const new_user = new User({ nickname: nickname, email: email, login: login, pwd: pwd });
+        new_user.save().then(() => console.log('Пользователь ' + nickname + ' создан!'));
+    },
+    get_user: function (filter, callback) {
+        Object.keys(filter).forEach(key => filter[key] == "" || filter[key] == null ? delete obj[key]:'');
+        User.find(filter, function (err, data) {
+            if (err) return console.log(err);
+            return callback(data);
+        })
+    },
+    get_char: function (filter, callback) {
+        Object.keys(filter).forEach(key => filter[key] == "" || filter[key] == null ? delete obj[key] : '');
+        Char.find(filter , function (err, data) {
+            if (err) return console.log(err);
+            return callback(data);
+        })
+    }
+
+};
--- server/server_core.js
+++ server/server_core.js
@@ -1,35 +1,64 @@
 ﻿// подключение express
 const express = require("express");
 const path = require('path');
+// подключение бд
+const db = require('./database/DB_manager');
 // создаем объект приложения
 const app = express();
-// Директория статики
-const BUILD_DIRECTORY = path.resolve(__dirname, '..', 'client')
-// Настройка статики
-app.use(express.static(BUILD_DIRECTORY));
+// Прошедшая 
+const yes = JSON.stringify({ result: true });
+const no = {
+    result: false
+};
+const name_is_taken = JSON.stringify({ result: "name is taken" })
+const login_is_taken = JSON.stringify({ result: "login is taken" })
+const email_is_taken = JSON.stringify({ result: "email is taken" })
+const acc_is_created = JSON.stringify({ result: "acc is created" });
 
-// определяем обработчики для маршрутов
-app.get("/", function (request, response) {
 
-        // отправляем ответ
-    response.sendFile(`${BUILD_DIRECTORY}/auth.html`);
-
+app.get('/methods/characterSheet', (req, res) => {
+    res.send(JSON.stringify(yes))
 });
-app.get("/login", function (request, response) {
 
-    // отправляем ответ
-    response.sendFile(`${BUILD_DIRECTORY}/log_in.html`);
-
-});
-app.get("/signup", function (request, response) {
-
-    // отправляем ответ
-    response.sendFile(`${BUILD_DIRECTORY}/sign_up.html`);
-
-});
 
 app.post('/create_acc', (req, res) => {
-    res.send('create_acc')
+    nickname = req.query['nickname']
+    email = req.query['email']
+    login = req.query['login']
+    pwd = req.query['pwd']
+    filter1 = {}
+    filter1.nickname = nickname
+    filter2 = {}
+    filter2.login = login
+    filter3 = {}
+    filter3.email = email
+    db.get_user(filter1, function (data) {
+        if (data[0] !== undefined) {
+            res.send(name_is_taken)
+            console.log("Поиск по имени нашел значения")
+        }
+        else
+        {
+            db.get_user(filter2, function (data2) {
+                if (data2[0] !== undefined) {
+                    res.send(login_is_taken)
+                    console.log("Поиск по логину нашел значения")
+                }
+                else {
+                    db.get_user(filter3, function (data3) {
+                        if (data3[0] !== undefined) {
+                            res.send(email_is_taken)
+                            console.log("Поиск по майлу нашел значения")
+                        }
+                        else {
+                            db.add_user(nickname, email, login, pwd)
+                            res.send(yes)
+                        }
+                    });
+                }
+            });
+        }
+    })    
 });
 
 app.post('/create_char', (req, res) => {
