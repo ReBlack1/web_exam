a04e0d68aef14a74de1ce67c12f0dc7821d24eae
ReBlack <reblack1@yandex.ru>
2019-05-27 19:06:37 +0200

    Попытка поправить автобилд в хероку

---
+++ server/database/DB_config.js
@@ -0,0 +1,11 @@
+﻿const login = "ReBlack1"
+const pwd = "684551qQ"
+
+module.exports = {
+    get_login: function() {
+        return "ReBlack1";
+    },
+
+     get_pwd: function() {
+        return "684551qQ";
+    }};
